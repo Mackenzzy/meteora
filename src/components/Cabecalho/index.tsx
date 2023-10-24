11115d61d2a5b83670c4d3913e75c84d84852891
Mackenzzy <mackenzzy@gmail.com>
2023-10-24 16:16:15 -0300

    cabecalho completo

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import CampodeBusca from "../CampoDeBusca";
+import "./style.scss";
 
 class Cabecalho extends React.Component {
     render() {
         return (
             <div className="cabecalho">
-                <img src="logo.png" alt="logo" />
-                <ul>
-                    <li>Home</li>
-                    <li>Nossas Lojas</li>
-                    <li>Novidade</li>
-                    <li>Promoções</li>
-                </ul>
-                <h1>React App</h1>
+                <div className="alinhamento">
+                <img src="./imagens/logo/logo-desktop.png" alt="logo" />
+                    
+                    <ul>
+
+                        <li> <a href="#">Home</a></li>
+                        <li><a href="#">Nossas Lojas</a></li>
+                        <li><a href="#">Novidade</a></li>
+                        <li><a href="#">Promoções</a></li>
+                    </ul>
+
+
+                    </div>
                 <CampodeBusca />
+                
             </div>
         );
     }
