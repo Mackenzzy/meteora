68205f29aa7150aedaac970a94073861fa2ac0ff
Mackenzzy <mackenzzy@gmail.com>
2023-10-24 22:37:25 -0300

    Banner adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Cabecalho from './components/Cabecalho';
+import Banner from './pages/Home/Banner';
 
 function App() {
   return (
     <div className="App">
      <Cabecalho>  </Cabecalho>
+     <Banner/>
     </div>
   );
 }
---
+++ src/pages/Home/Banner/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.scss";
+
+class Banner extends React.Component {
+    render() {
+        return (
+            <div className="Banner">
+                <img src="./imagens/Banners/BannerCarrosel1.png" alt="" />
+                 
+            </div>
+        );
+    }
+}
+
+export default Banner;
