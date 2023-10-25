2b1f86d693dd40c5d9f17ea86941c15f130e23e4
Mackenzzy <mackenzzy@gmail.com>
2023-10-25 15:27:18 -0300

    Secao de facilidades concluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Cabecalho from './components/Cabecalho';
+import Facilidades from './components/Facilidades';
 import ListaCategorias from './components/ListaCategorias';
 import Banner from './pages/Home/Banner';
 
@@ -10,6 +11,7 @@ function App() {
      <Cabecalho>  </Cabecalho>
      <Banner/>
      <ListaCategorias/>
+     <Facilidades/>
     </div>
   );
 }
