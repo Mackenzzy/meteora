a87754f479b93772c7f809a7e2955f436cb30e56
Mackenzzy <mackenzzy@gmail.com>
2023-10-25 21:16:04 -0300

    lista de produtos e section concluidas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import Cabecalho from './components/Cabecalho';
-import CardProdutos from './components/CardProdutos';
+
 import Facilidades from './components/Facilidades';
 import ListaCategorias from './components/ListaCategorias';
+import ListaProdutos from './components/ListaProtudos';
 import Banner from './pages/Home/Banner';
 
 function App() {
@@ -12,8 +13,10 @@ function App() {
      <Cabecalho>  </Cabecalho>
      <Banner/>
      <ListaCategorias/>
+     <ListaProdutos/>
      <Facilidades/>
-     <CardProdutos/>
+
+
     </div>
   );
 }
--- src/components/CardProdutos/index.tsx
+++ src/components/CardProdutos/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import './style.scss';
 
-class CardProdutos extends React.Component {
-    render() {
-        return <div>CardProdutos</div>;
+interface Props{
+    imagem:string,
+    altImagem: string,
+    nome:string,
+    descricao:string,
+    preco:string
+}
+
+
+const CardProdutos: React.FC<Props> = ({imagem, altImagem, nome, descricao, preco}) => { {
+        return (
+        <div className="cardProdutos">
+            <img src={imagem} alt={altImagem} />
+            <h2>{nome}</h2>
+            <p>{descricao}</p>
+            <h3>R$ {preco}</h3>
+            <button>Ver Mais</button>
+        </div>);
     }
 }
 
