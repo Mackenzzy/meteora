ad8b6ab7cb7794ecf39d49301b291a0fcf39d46b
Mackenzzy <mackenzzy@gmail.com>
2023-10-27 15:45:55 -0300

    input radio

--- src/components/ModalProduto/index.tsx
+++ src/components/ModalProduto/index.tsx
@@ -25,10 +25,16 @@ export default function Modal({ isOpen, setIsOpen }: IModal) {
                             <form action="">
 
                                 <h2>{produto.nome}</h2>
-                                <p>{produto.descricao}</p>
-                                <h3>R$ {produto.preco}</h3>
+                                <p>{produto.descricao}
+                                <br />
+                                <hr />
+                                </p>
+                                <h3>R${produto.preco}</h3>
 
-                                <h4>Vendido e entregue por Riachuelo</h4>
+                                <p className="entregue">Vendido e entregue por Riachuelo
+                                <br />
+                                <hr />
+                                </p>
                                 <p>Cores</p>
                                 <div className="cores">
                                     <input type="radio" id="azul-claro" name="cor" value="Azul Claro" />
@@ -39,6 +45,27 @@ export default function Modal({ isOpen, setIsOpen }: IModal) {
 
                                     <input type="radio" id="preto" name="cor" value="Preto" />
                                     <label htmlFor="preto">Preto</label>
+                                    <br />
+                                    <hr />
+                                </div>
+                                <p>Tamanho</p>
+                                <div className="cores">
+                                    <input type="radio" id="P" name="cor" value="Azul Claro" />
+                                    <label htmlFor="azul-claro">P</label>
+
+                                    <input type="radio" id="PP" name="cor" value="Offwhite" />
+                                    <label htmlFor="offwhite">PP</label>
+
+                                    <input type="radio" id="M" name="cor" value="Preto" />
+                                    <label htmlFor="preto">M</label>
+
+                                    <input type="radio" id="G" name="cor" value="Preto" />
+                                    <label htmlFor="preto">G</label>
+
+                                    <input type="radio" id="GG" name="cor" value="Preto" />
+                                    <label htmlFor="preto">GG</label>
+                                    <br />
+                                    <hr />
                                 </div>
 
                                 <BotaoRoxo
